acaf3ad3579e9e82d3405526d8ac4cf5fd0c82d6
Jul Lang <jullanggit@proton.me>
2025-05-07 20:13:14 +0200

    finish manual filtering and tune filtered values

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,45 @@
-use std::{array, char, env, fmt::Debug, fs, path::PathBuf};
+use std::{array, env, fmt::Debug, fs, path::PathBuf};
 
 use plotters::{
     chart::ChartBuilder,
     prelude::{IntoDrawingArea, IntoSegmentedCoord, SVGBackend},
     series::Histogram,
     style::{
-        Color,
-        full_palette::{RED, WHITE},
+        Color, ShapeStyle,
+        full_palette::{BLUE, RED, WHITE},
     },
 };
 
 #[derive(Clone, PartialEq)]
 struct CustomDebugString(String);
+impl CustomDebugString {
+    fn cleaned(str: &str) -> Self {
+        Self(
+            str.char_indices()
+                .map(|(position, char)| match char {
+                    '\0' => "\\0",
+                    '\u{1}' => "^A", // heading
+                    '\u{3}' => "^C",
+                    '\u{8}' => "\\b", // backspace
+                    '\u{9}' => "\\t", // tab
+                    '\u{12}' => "^R", // device-control 2
+                    '\u{14}' => "^T", // device-control 4
+                    '\u{16}' => "^V",
+                    '\u{17}' => "^W",
+                    '\u{18}' => "^X",  // cancel
+                    '\u{1B}' => "\\e", // escape
+                    '\u{7F}' => "\\d", // delete
+                    ' ' | '\u{A0}' => "\\s",
+                    other if other.is_control() => "�",
+                    other => &str[position..(position + other.len_utf8())],
+                })
+                .collect::<String>(),
+        )
+    }
+}
 impl Debug for CustomDebugString {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let cleaned = self
-            .0
-            .replace("\u{8}", "\\b")
-            .replace("\u{9}", "\\t")
-            .replace("\u{1B}", "\\e")
-            .replace("\u{7F}", "\\d");
-        println!("{}", cleaned.escape_default());
-        write!(f, "{}", cleaned.escape_default())
+        write!(f, "{}", self.0)
     }
 }
 
@@ -38,7 +56,7 @@ fn main() {
             .map(|(number, entry)| {
                 (
                     number.parse::<u32>().unwrap(),
-                    CustomDebugString(entry.to_string()),
+                    CustomDebugString::cleaned(entry),
                 )
             })
             .collect::<Vec<_>>();
@@ -47,18 +65,31 @@ fn main() {
         ngrams
     });
 
-    unique_ngrams(&ngrams, "");
-    num_per_ngram(&ngrams, "");
+    let keylogger_style = RED.mix(0.5).filled();
+    let reference_style = BLUE.mix(0.5).filled();
 
-    for ngrams in &mut ngrams {
-        ngrams.retain(|(num, _)| *num > 10)
+    unique_ngrams(&ngrams, "", keylogger_style);
+    num_per_ngram(&ngrams, "", keylogger_style);
+
+    for (n, ngrams) in ngrams.iter_mut().enumerate() {
+        let len = match n {
+            0 => 80,
+            1 => 115,
+            2 => 90,
+            _ => unreachable!(),
+        };
+        ngrams.truncate(len);
     }
 
-    unique_ngrams(&ngrams, " (filtered)");
-    num_per_ngram(&ngrams, " (filtered)");
+    unique_ngrams(&ngrams, " (filtered)", keylogger_style);
+    num_per_ngram(&ngrams, " (filtered)", keylogger_style);
 }
 
-fn unique_ngrams(ngrams: &[Vec<(u32, CustomDebugString)>; 3], modifiers: &str) {
+fn unique_ngrams(
+    ngrams: &[Vec<(u32, CustomDebugString)>; 3],
+    modifiers: &str,
+    style: impl Into<ShapeStyle> + Clone,
+) {
     let max = ngrams.iter().map(|ngram| ngram.len()).max().unwrap();
 
     let path = format!("target/Unique N-Grams{modifiers}.svg");
@@ -99,7 +130,11 @@ fn unique_ngrams(ngrams: &[Vec<(u32, CustomDebugString)>; 3], modifiers: &str) {
     root.present().unwrap();
 }
 
-fn num_per_ngram(ngrams: &[Vec<(u32, CustomDebugString)>; 3], modifiers: &str) {
+fn num_per_ngram(
+    ngrams: &[Vec<(u32, CustomDebugString)>; 3],
+    modifiers: &str,
+    style: impl Into<ShapeStyle> + Clone,
+) {
     for n in 0..ngrams.len() {
         let max = ngrams[n].iter().map(|(num, _)| num).max().unwrap();
 
@@ -136,9 +171,14 @@ fn num_per_ngram(ngrams: &[Vec<(u32, CustomDebugString)>; 3], modifiers: &str) {
         chart
             .draw_series(
                 Histogram::vertical(&chart)
-                    .style(RED.filled())
+                    .style(style.clone())
                     .margin(0)
-                    .data(ngrams[n].iter().map(|(num, ngram)| (ngram, *num))),
+                    .data(
+                        ngrams[n]
+                            .iter()
+                            .filter(|(_, ngram)| ngram.0 != "�")
+                            .map(|(num, ngram)| (ngram, *num)),
+                    ),
             )
             .unwrap();
 
